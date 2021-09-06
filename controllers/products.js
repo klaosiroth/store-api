26958356324b7f66730d98074a7bd5f2d14bfdf0
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-07 02:43:31 +0700

    Sort - Implementation

--- controllers/products.js
+++ controllers/products.js
@@ -10,6 +10,7 @@ const getAllProducts = async (req, res) => {
     featured,
     company,
     name,
+    sort,
   } = req.query
   const queryObject = {}
 
@@ -27,7 +28,19 @@ const getAllProducts = async (req, res) => {
   }
   console.log(queryObject)
 
-  const products = await Product.find(queryObject)
+  let result = Product.find(queryObject)
+  
+  // sort
+  if (sort) {
+    console.log(sort)
+    const sortList = sort.split(',').join(' ')
+    console.log(sortList)
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createAt')
+  }
+
+  const products = await result
   res.status(200).json({ nbHits: products.length, products })
 }
 
