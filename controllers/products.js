ffd6143fa907126c2844691db9084004d713b619
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-07 03:15:33 +0700

    Pagination

--- controllers/products.js
+++ controllers/products.js
@@ -54,6 +54,14 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList)
   }
 
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  // all products => 23
+  // divided by 7 => 4 pages - each page: 7 7 7 2
+  result = result.skip(skip).limit(limit)
+
   const products = await result
   res.status(200).json({ nbHits: products.length, products })
 }
