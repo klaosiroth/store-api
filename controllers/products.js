f3f92b3744c1f317d7a99f471070fc341ed4168e
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-07 01:57:02 +0700

    Refactor to QueryObject

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,23 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({
-    
+    featured: true,
   })
   res.status(200).json({ nbHits: products.length, products })
 }
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query)
+  const { 
+    featured,
+  } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  console.log(queryObject)
+
+  const products = await Product.find(queryObject)
   res.status(200).json({ nbHits: products.length, products })
 }
 
